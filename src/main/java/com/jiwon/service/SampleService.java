43f8d6446fa022657acc6bf7c2ab05142a088166
aidencjswo@gmail.com <aidencjswo@gmail.com>
2023-06-28 23:43:21 +0900

    as

--- src/main/java/com/jiwon/service/SampleService.java
+++ src/main/java/com/jiwon/service/SampleService.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface SampleService {
 
+    public void serviceTest();
 
 }
--- src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
+++ src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class SampleServiceImpl implements SampleService {
+    @Override
+    public void serviceTest() {
 
+    }
 }
