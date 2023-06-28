c27018f1654910cbf11128fff217cb445f405168
aidencjswo@gmail.com <aidencjswo@gmail.com>
2023-06-28 23:44:29 +0900

    as

--- src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
+++ src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
@@ -1,12 +1,14 @@
 package com.jiwon.serviceImpl;
 
 import com.jiwon.service.SampleService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+@Log4j2
 @Repository
 public class SampleServiceImpl implements SampleService {
     @Override
     public void serviceTest() {
-
+        log.info("asdfs");
     }
 }
