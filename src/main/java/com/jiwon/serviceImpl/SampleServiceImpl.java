38aa6cfbb1ebb4e0119d6a3014fb564459579370
sangyeong <aidencjswo@gmail.com>
2023-06-29 14:53:37 +0900

    jenkins test

--- src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
+++ src/main/java/com/jiwon/serviceImpl/SampleServiceImpl.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 public class SampleServiceImpl implements SampleService {
     @Override
     public void serviceTest() {
-        log.info("asdfs");
+        log.info("jenkins test");
     }
 }
