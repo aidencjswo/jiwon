8e45de388b816d6b11c2ea96b88d7b0dc25ee9fd
sangyeong <aidencjswo@gmail.com>
2023-06-30 16:59:54 +0900

    jpa 개발 중

---
+++ src/main/java/com/jiwon/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.jiwon.repository;
+
+import com.jiwon.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+}
