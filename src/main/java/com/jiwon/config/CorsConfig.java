f8083c060853288dee11eb0ad404306e2776acde
sangyeong <aidencjswo@gmail.com>
2023-07-13 20:49:54 +0900

    cors 수정

--- src/main/java/com/jiwon/config/CorsConfig.java
+++ src/main/java/com/jiwon/config/CorsConfig.java
@@ -5,13 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class sampleConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 출처
+                .allowedOriginPatterns("http://121.131.135.84:3000") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
+
 }
